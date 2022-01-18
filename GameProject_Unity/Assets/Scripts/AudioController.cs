a79b3b9a36569269c27f25853c793c45da951b1b
Lotka <wikisophie@gmail.com>
2022-01-18 19:34:30 +0100

    Add StepSound (don't work!)

--- GameProject_Unity/Assets/Scripts/AudioController.cs
+++ GameProject_Unity/Assets/Scripts/AudioController.cs
@@ -4,36 +4,13 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    int number = 1;
-    float floatNumber = 2.34f;
-    string text = "to jest tekst";
-    bool logical = true;
-    int[] intArray;
-    int[] intArray2 = new int[] {1,2,3,4};
-   // int[] intArray3 = {1, 2, 3, 4};
+    public AudioClip stepClip;
 
-    List<int> list = new List<int>();
+    public AudioSource audioSource;
+  
 
-   private int MyMethod()
+    public void Step()
     {
-        return number;
-    }
-    
-    private void Awake()
-    {
-        Debug.Log("Awake");
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Start" + MyMethod());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        audioSource.PlayOneShot(stepClip);
     }
 }
-
