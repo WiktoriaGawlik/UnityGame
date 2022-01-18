82d8ca0dc8bec5b582d3643c435404c9e0ff4d99
Lotka <wikisophie@gmail.com>
2022-01-18 16:49:26 +0100

    Add PauseMenu

---
+++ GameProject_Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Reasume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Reasume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
